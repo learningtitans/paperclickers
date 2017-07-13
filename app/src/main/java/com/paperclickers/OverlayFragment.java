f1cad3ac1acae3ff5777bc16065b11586fe02d09
Eduardo Seiti de Oliveira <eduseiti@gmail.com>
2017-07-13 07:37:39 -0300

    Initial fragment tests

---
+++ app/src/main/java/com/paperclickers/OverlayFragment.java
@@ -0,0 +1,20 @@
+package com.paperclickers;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by eduseiti on 13/07/17.
+ */
+
+public class OverlayFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.overlay_fragment, container, false);
+    }}
